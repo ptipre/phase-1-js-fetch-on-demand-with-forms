6de8c54795458f1855c4f8106f6c82b76f1ac3bc
Prajay Tipre <prajaytipre@gmail.com>
2021-11-11 12:07:25 -0500

    commit tree 714dc4705d286ffb4f52d80e2d35776932d0b40d

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 const init = () => {
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', e => {
+
+        e.preventDefault();
+        let id = e.target.children[1].value;
+        
+        fetch(`http://localhost:3000/movies/${id}`)
+        .then(resp => resp.json())
+        .then(data => {
+
+            const title = document.querySelector('h4');
+            const summary = document.querySelector('p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+
+            form.reset();
+
+        })
+        .catch(error => {
+            console.error(error);
+            alert('The id you have entered is invalid');
+        })
+    })
+
   
 }
 
